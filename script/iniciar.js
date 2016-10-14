54177843d96bb74aed1076f4eeed4ef78b305f13
sethiris <sethiris@hotmail.es>
2016-10-14 18:54:57 -0300

    Agregue Jquery mobile, css, y maquetado html
    diagramar las primeras pantallas, y esbozo de funcion login y logout

--- script/iniciar.js
+++ script/iniciar.js
@@ -1,6 +1,19 @@
 $('document').ready(iniciarPrograma);
 
 function iniciarPrograma(){
+      $("#divMenu").hide();
+      $("#btnLogin").click(login);
+      $("#Logout").click(logout);
+}
 
+function login(){
+  $("#divMenu").show();
+  $("#divLogin").hide();
+
+}
+
+function logout(){
+  $("#divMenu").hide();
+  $("#divLogin").show();
 
 }
